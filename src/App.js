0c32d66d2d9ca256688fbfefbd6f9d06896ebfb2
Khawla-bk <benkadidakhawla@gmail.com>
2022-02-27 19:27:42 +0100

    added mui package

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import Footer from "./Components/Footer/Footer";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {" "}
+      <Routes>
+        {/* footer visible by link */}
+        <Route path="/footer" element={<Footer />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
