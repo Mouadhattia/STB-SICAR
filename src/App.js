df26c332d13cac0d2326790d1a3f10f3b79083b0
lwis1 <lwis1992@gmail.com>
2022-02-27 19:20:08 +0100

    nosFond html added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import "./App.css";
+import NosFond from "./nosFonds/NosFond";
 
 function App() {
-  return <div className="App">fffff</div>;
+  return <div className="App">
+    <NosFond></NosFond>
+    </div>;
 }
 
 export default App;
